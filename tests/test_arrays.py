d02499ea8e03ca000ce330a17328c312a5ab96e6
Jonas Neubert <jonas@counsyl.com>
2015-04-06 10:34:17 -0700

    use pytest fixtures

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -1,37 +1,61 @@
+import pytest
+
 from counsyl_pyads.adsdatatypes import AdsArrayDatatype
 from counsyl_pyads.adsdatatypes import INT
 
 
+@pytest.fixture
+def dims_1dim():
+    return [(1, 3)]
+
+
+@pytest.fixture
+def flat_1dim():
+    return (9, 8, 7)
+
+
+@pytest.fixture
+def dict_1dim():
+    return {1: 9, 2: 8, 3: 7}
+
+
+@pytest.fixture
+def dims_2dim():
+    return [(1, 2), (3, 4)]
+
+
+@pytest.fixture
+def flat_2dim():
+    return (1, 2, 3, 5)
+
+
+@pytest.fixture
+def dict_2dim():
+    return {1: {3: 1, 4: 2}, 2: {3: 3, 4: 5}}
+
+
 class TestArray(object):
 
-    dims_1dim = [(1, 3)]
-    flat_1dim = (9, 8, 7)
-    dict_1dim = {1: 9, 2: 8, 3: 7}
-
-    dims_2dim = [(1, 2), (3, 4)]
-    flat_2dim = (1, 2, 3, 5)
-    dict_2dim = {1: {3: 1, 4: 2}, 2: {3: 3, 4: 5}}
-
-    def test_flatten_1dim(self):
-        arr = AdsArrayDatatype(INT, self.dims_1dim)
-        dict_ = self.dict_1dim
+    def test_flatten_1dim(self, dims_1dim, flat_1dim, dict_1dim):
+        arr = AdsArrayDatatype(INT, dims_1dim)
+        dict_ = dict_1dim
         flat = arr._dict_to_flat_list(dict_)
-        assert(flat == self.flat_1dim)
+        assert(flat == flat_1dim)
 
-    def test_flatten_2dim(self):
-        arr = AdsArrayDatatype(INT, self.dims_2dim)
-        dict_ = self.dict_2dim
+    def test_flatten_2dim(self, dims_2dim, flat_2dim, dict_2dim):
+        arr = AdsArrayDatatype(INT, dims_2dim)
+        dict_ = dict_2dim
         flat = arr._dict_to_flat_list(dict_)
-        assert(flat == self.flat_2dim)
+        assert(flat == flat_2dim)
 
-    def test_unflatten_1dim(self):
-        arr = AdsArrayDatatype(INT, self.dims_1dim)
-        flat = self.flat_1dim
+    def test_unflatten_1dim(self, dims_1dim, flat_1dim, dict_1dim):
+        arr = AdsArrayDatatype(INT, dims_1dim)
+        flat = flat_1dim
         dict_ = arr._flat_list_to_dict(flat)
-        assert(dict_ == self.dict_1dim)
+        assert(dict_ == dict_1dim)
 
-    def test_unflatten_2dim(self):
-        arr = AdsArrayDatatype(INT, self.dims_2dim)
-        flat = self.flat_2dim
+    def test_unflatten_2dim(self, dims_2dim, flat_2dim, dict_2dim):
+        arr = AdsArrayDatatype(INT, dims_2dim)
+        flat = flat_2dim
         dict_ = arr._flat_list_to_dict(flat)
-        assert(dict_ == self.dict_2dim)
+        assert(dict_ == dict_2dim)
