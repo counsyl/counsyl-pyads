62f2eb5e51367492155e231a16aee906077e2144
Jonas Neubert <jonas@counsyl.com>
2015-08-18 22:47:38 -0700

    Use read_length information correctly, remove as property in AdsSymbol
    ReadLength was incorrectly treated as symbol property, this commit fixes this.

--- counsyl_pyads/adssymbol.py
+++ counsyl_pyads/adssymbol.py
@@ -1,8 +1,6 @@
 class AdsSymbol(object):
     def __init__(
-            self, read_length, index_group, index_offset, name, symtype,
-            comment):
-        self.read_length = read_length
+            self, index_group, index_offset, name, symtype, comment):
         self.index_group = index_group
         self.index_offset = index_offset
         self.name = name
