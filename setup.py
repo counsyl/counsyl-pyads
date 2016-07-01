ff49c0a096fb095d9a08ebd1498bb43fae627df7
Jonas Neubert <jonas@counsyl.com>
2016-06-30 22:56:10 -0700

    update setup.py url to prospective public Github url

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
     scripts=['bin/twincat_plc_info.py'],
     include_package_data=True,
     zip_safe=False,
-    url='https://github.counsyl.com/dev/counsyl_pyads.git',
     description=(
         'A library for directly interacting with a Twincat PLC. Based on '
         'https://github.com/chwiede/pyads.'
     ),
     author='Counsyl Inc.',
     author_email='opensource@counsyl.com',
+    url='https://github.com/counsyl/counsyl_pyads/',
 )
